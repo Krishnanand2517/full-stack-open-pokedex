f8ec31594e5a047498af2d412a10fd60ac278249
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-07-26 23:52:34 +0530

    Start server in playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -29,6 +29,13 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    baseURL: 'http://localhost:5000/',
+  },
+  webServer: {
+    command: 'npm run start-prod',
+    url: 'http://127.0.0.1:5000',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
   },
 
   /* Configure projects for major browsers */
