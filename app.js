23de23305467f0b835718543c96e2e74d50e73eb
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-07-27 00:44:13 +0530

    Introduce intended error in health check

--- app.js
+++ app.js
@@ -11,10 +11,12 @@ app.get('/version', (_req, res) => {
 })
 
 app.get('/health', (_req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  '
   res.send('ok')
 })
 
 app.listen(PORT, () => {
-  /* eslint-disable no-console */
+  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
